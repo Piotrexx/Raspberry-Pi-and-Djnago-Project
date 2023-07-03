03c332e8eec42e3466d9fa37f93f7e890e4b69d1
Peter <piotr.lopatecki@gmail.com>
2023-07-03 20:25:24 +0200

    date fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,20 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const [data, setData] = useState([])
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZoneName: 'short'
+  };
+  function formatDate(date) {
+    const dateTime = new Date(date);
+    return dateTime.toLocaleString(undefined, options);
+  }
+
 
   useEffect(() => {
     fetch('http://localhost:8000/api/temperature/')
@@ -25,11 +39,10 @@ function App() {
               {item.temperature}
             </li>
             <li>
-              {item.date_created}
+              {formatDate(item.date_created)}
             </li>
           </ul>
         ))
-    
       }
     </div>
   );
